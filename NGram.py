56ff1eeef10a08ff37b89f7e992f37533e7b5122
Eric Bell <ewbell94@gmail.com>
2017-12-12 18:19:24 -0500

    NGram started

---
+++ NGram.py
@@ -0,0 +1,14 @@
+#Written by Eric Bell
+#12/12/17
+#
+#NGram.py generates a melody based off of an NGram model from midi files
+
+import random
+
+def merge
+def genNgram(n,files):
+    return {A:"A"}
+    
+def genMelody(n):
+    return [("C1",4)]
+
